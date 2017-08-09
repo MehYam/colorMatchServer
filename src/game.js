29c24f12e12c9c9270fa9c2d60a1ea09c64856c6
Kai Arnold <cdn.kai@gmail.com>
2017-08-09 16:59:02 -0700

    show game id in game rendering

--- src/game.js
+++ src/game.js
@@ -3,7 +3,7 @@ const ReactDOM = require('react-dom');
 
 class Game extends React.Component {
    render() {
-      return <div>This is the game board</div>;
+      return <div>This is the game board for {this.props.match.params.gameid}</div>;
    }
 }
 
